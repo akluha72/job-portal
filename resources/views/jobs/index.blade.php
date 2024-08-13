188622674cd13668a20b7eb3bc301f75ceb01623
lutfi_akmal <lutfiakmal@myra.com.my>
2024-08-13 14:05:43 +0800

    jobs page and card component

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,7 +1,8 @@
 <x-layout>
     @foreach($jobs as $job)
-        <div class="title">
+        <x-card class="mb-4">
             {{ $job->title }}
-        </div>
+        </x-card>
     @endforeach
 </x-layout>
+
