e88db5a1d26b2e6611fe833ccbbc04f0c2385bd3
lutfi_akmal <lutfiakmal@myra.com.my>
2024-08-13 14:25:21 +0800

    Jobs Page: Tag component & Job info

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->class(['rounded-md border border-slate-300 bg-white p-4 shadow-sm mb-4'])}}>
+<article {{ $attributes->class(['rounded-md border border-slate-300 bg-white p-4 shadow-sm mb-4'])}}>
     {{ $slot }}
-</div>
+</article>
