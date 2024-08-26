e19dfb7e787803d1266d24e0cfee5e5baa83ca85
lutfi_akmal <lutfiakmal@myra.com.my>
2024-08-26 14:18:52 +0800

    factory and seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,11 +8,25 @@ use App\Models\Job;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+  
     public function run(): void
     {
+        \App\Models\User::factory(300)->create();
+        $users = \App\Models\User::all()->shuffle();
+        //associate user with employer, user can be only one employer.
+        for ($i = 0; $i< 20; $i++){
+            \App\Models\Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = \App\Models\Employer::all();
+
+        for($i = 0; $i < 100; $i++){
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
         job::factory(100)->create();
     }
 }
